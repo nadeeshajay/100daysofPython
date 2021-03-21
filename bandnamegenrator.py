2a31dd9b288a40a5bafdb41e04621d3dd31f2dfd
Nadeesha Jayasooriya <80101306+nadeeshajay@users.noreply.github.com>
2021-03-21 23:51:01 +0530

    pythonDay1

---
+++ bandnamegenrator.py
@@ -0,0 +1,4 @@
+print('Welcome to the band name genarator')
+city = input('What\'s the name of city you grew up ? ')
+pet = input('What\'s your pet\'s name ? ')
+print('The name of your band is.... \n' + city + ' ' + pet)
